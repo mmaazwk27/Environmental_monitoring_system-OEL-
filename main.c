0fdbba006130eb4f5c932fc50ef3187f8872fa70
Muhammad Maaz Wali Khan <mmaazwalik@gmail.com>
2024-11-22 23:55:56 +0500

    main.c

--- main.c
+++ main.c
@@ -6,32 +6,7 @@
 #include <curl/curl.h>
 #include "cJSON.h"
 #include "header/data.h"
-// #define API_KEY "e5e24f6fafa622984f7505534f34942b"
-// #define API_URL "http://api.openweathermap.org/data/2.5/weather"
-// #define LATITUDE "24.860966"
-// #define LONGITUDE "66.990501"
-// #define RAW_DATA_FILE "raw_data.json"
-// #define ORGANIZED_DATA_FILE "organize_data.txt"//raw from json
-// #define PROCESSED_DATA_FILE "processed_data.txt"//from organize 
 
-// #define MAX_WEATHER_ENTRIES 3
-// #define MAX_LINE_LENGTH 20000
-
-
-
-// int main() {
-    
-    
-//                 struct MemoryStruct responseData;
-//                 retrieveWeatherData(&responseData);
-//                 saveOrganizedDataToFile(responseData.memory);
-//                 free(responseData.memory);
-            
-//                 processAndSaveData();
-
-                
-
-// }
 
 int main() {
     struct MemoryStruct responseData;
